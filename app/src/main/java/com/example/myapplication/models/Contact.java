a196f8a78449fdc9a6e25e2ffa367d92485a8519
AuliaNurachman <aulianurachman388@gmail.com>
2019-02-21 20:28:21 +0700

    recycle view

--- app/src/main/java/com/example/myapplication/models/Contact.java
+++ app/src/main/java/com/example/myapplication/models/Contact.java
@@ -18,7 +18,16 @@ public class Contact {
     public boolean isOnline() {
         return mOnline;
     }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
     }
-
-
-}
+    }
